4ff250ac75a26ce63d9ed0623394e8b8aa0e76cf
seixasmaycon <145515798+seixasmaycon@users.noreply.github.com>
2023-09-22 12:46:50 -0300

    Update Ordena a sequência de números

--- Lista-00/Tópico 6 - Vetores/Questão 110.c
+++ Lista-00/Tópico 6 - Vetores/Questão 110.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int main(){
+  int A[11] = {1,2,3,4,5,6,7,8,9,10,11},temp;
+  for(int i=0; i<11; i++){
+    if(A[5]<A[i]){
+      temp = A[5];
+      A[5]=A[i];
+      A[i]=temp;
+    }
+    if(A[i]>A[i+1]&&i < 9){
+      temp = A[i];
+      A[i]=A[i+1];
+      A[i+1]=temp;
+    }
+    for(int i=6; i<11; i++){
+      if(A[i]<A[i+1]&&i < 10){
+        temp = A[i];
+        A[i]=A[i+1];
+        A[i+1]=temp;
+      }
+    }
+  }
+
+  
+  for(int i=0; i<11; i++){
+    printf("%d ", A[i]);
+  }
+  return 0;
+ 
+}
