8971ce765089b2b29e75df95a34135f2edddb9e1
laralararissa <131396340+laralararissa@users.noreply.github.com>
2023-09-23 17:12:55 -0300

    Crie um programa que lê 6 valores inteiros e, em seguida, mostre na tela os valores lidos.

---
+++ Lista-00/Tópico 6 - Vetores/exercicio-74.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int valores[6];
+    int i;
+
+  
+    printf("Digite 6 valores inteiros:\n");
+
+    for (i = 0; i < 6; i++) {
+        printf("Valor %d: ", i + 1);
+        scanf("%d", &valores[i]);     
+}
+       printf("Valores lidos:\n");
+    for (i = 0; i < 6; i++) {
+        printf("%d\n", valores[i]);
+    }
+
+    return 0;
+}
